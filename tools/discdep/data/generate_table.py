f8e1e8b6215c6fa65e195573037726976337614d
norikinishida <norikinishida@gmail.com>
2021-10-24 19:43:40 +0900

    examples.csvを追加

---
+++ tools/discdep/data/generate_table.py
@@ -0,0 +1,57 @@
+import io
+import json
+import os
+
+import utils
+
+
+RELATIONS = [
+    "ROOT",
+    "ELABORATION",
+    "ADDITION",
+    "COMPARISON",
+    "CAUSE-RESULT",
+    "CONDITION",
+    "TEMPORAL",
+    "ENABLEMENT",
+    "MANNER-MEANS",
+    "BACKGROUND",
+    "FINDINGS",
+    "TEXTUAL-ORGANIZATION",
+    "SAME-UNIT",
+]
+
+
+def main():
+    filenames = utils.read_lines("./examples.txt")
+    filenames.sort()
+
+    table = {r: [] for r in RELATIONS}
+    for filename in filenames:
+        table = update_table(table, "./examples", filename)
+
+    with open("./examples.csv", "w") as f:
+        for rel in RELATIONS:
+            names = list(set(table[rel]))
+            names.sort()
+            line = ",".join([rel] + names)
+            f.write("%s\n" % line)
+
+
+def update_table(table, dir_path, filename):
+    with io.open(os.path.join(dir_path, filename), encoding="utf_8_sig") as f:
+        line = f.read()
+        data = json.loads(line)
+    for edu in data["root"]:
+        # head = edu["parent"]
+        # dep = edu["id"]
+        rel = edu["relation"]
+        if rel == "null":
+            continue
+        table[rel].append(filename)
+    return table
+
+
+if __name__ == "__main__":
+    main()
+
